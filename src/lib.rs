becd019e8de2bfe4648316fc2eb10ced64988afe
spieljs <spieljs@gmail.com>
2019-01-06 02:52:26 +0100

    creating headers anotations

---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+#[macro_use]
+extern crate serde_derive;
+extern crate serde;
+extern crate serde_json;
+
+mod network_objects;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
