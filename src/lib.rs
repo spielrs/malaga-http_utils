e545307dfc80921c58d493c01ef70861b2cec083
spieljs <spieljs@gmail.com>
2019-01-08 01:49:26 +0100

    use bincode to serialize and deserialize to binary

--- src/lib.rs
+++ src/lib.rs
@@ -7,13 +7,14 @@ extern crate serde_derive;
 
 extern crate serde_json;
 extern crate serde;
+extern crate bincode;
 
 pub mod utils;
 
 ///trait to serialize objects networks
 pub trait NetworkObj {
-    fn deserialize_network<'a>(network_obj: Self) -> &'a[u8];
-    fn serialize_network(network_bin: &[u8]) -> Self;
+    fn serialize_to_struct(network_bin: &[u8]) -> Self;
+    fn serialize_to_binary(network_obj: Self) -> Vec<u8>;
 }
 
 #[cfg(test)]
@@ -22,17 +23,17 @@ mod tests {
     use std::str;
 
     impl NetworkObj for TestReq {
-        fn serialize_network(req: &[u8]) -> Self {
+        fn serialize_to_struct(req: &[u8]) -> Self {
             let req_string = str::from_utf8(req).unwrap();
             let ser_req: TestReq = serde_json::from_str(req_string).unwrap();
             
             ser_req
         }
 
-        fn deserialize_network<'a>(req: TestReq) -> &'a[u8] {
-            let des_req: &[u8] = serde::de::Deserialize::deserialize(req).unwrap();
+        fn serialize_to_binary(req: TestReq) -> Vec<u8> {
+            let ser_req: Vec<u8> = bincode::serialize(&req).unwrap();
 
-            des_req
+            ser_req
         } 
 }
 
@@ -57,11 +58,11 @@ mod tests {
     }
 
     #[test]
-    fn it_should_deserialize_request() {
+    fn it_should_serialize_to_strcut() {
 
         let resq = b"{
             \"headers\": {
-                \"Content-Type\": \"Application\",
+                \"Content-Type\": \"application/json\",
                 \"Authorization\": \"Basis 1ddmcdd\"
             },
             \"method\": \"POST\",
@@ -70,10 +71,33 @@ mod tests {
             }
         }";
 
-        let resq_deserialized: TestReq = TestReq::deserialize_network(resq);
-        let method = utils::get_method(resq_deserialized.method);
+        let resq_serialized: TestReq = TestReq::serialize_to_struct(resq);
+        let method = utils::get_method(resq_serialized.method);
 
-        assert_eq!(resq_deserialized.body.user, "test");
+        assert_eq!(resq_serialized.body.user, "test");
         assert_eq!(method, "POST");
+    }
+
+    #[test]
+    fn it_should_serializa_to_binary() {
+        let resq = b"{
+            \"headers\": {
+                \"Content-Type\": \"application/json\",
+                \"Authorization\": \"Basis 1ddmcdd\"
+            },
+            \"method\": \"POST\",
+            \"body\": {
+                \"user\":\"test\"
+            }
+        }";
+
+        let resq_struct_ser: TestReq = TestReq::serialize_to_struct(resq);
+
+        println!("struct: {:#?}", resq_struct_ser);
+
+        let resq_binary_ser: Vec<u8> = TestReq::serialize_to_binary(resq_struct_ser);
+
+        let req_string: String = bincode::deserialize(&resq_binary_ser[..]).unwrap();
+        println!("binary {:#?}", req_string);
     }
 }
